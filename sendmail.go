dbd2856d7eba2f7f1f29fa2382e9931c01fb13c9
Andrea Cattaneo <andrea.cattaneo.dev@gmail.com>
2017-02-09 22:22:34 +0100

    add toml configuration file support ( that don't work )

--- sendmail.go
+++ sendmail.go
@@ -1,49 +1,139 @@
 /*
  * HOW TO BUILD:
  * > go get gopkg.in/gomail.v2
+ * > go get github.com/BurntSushi/toml
  * > go build
  */
 package main
 
 import (
-    "flag"
-    "gopkg.in/gomail.v2"
-    "fmt"
-    "bufio"
-    "os"
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/BurntSushi/toml"
+	"gopkg.in/gomail.v2"
 )
 
-func main() {
-    var sender = flag.String("from", "sender@example.com", "sender email address")
-    var recipient = flag.String("to", "recipient@example.com", "recipient email address")
-    var smtp_server_address = flag.String("server", "aspmx.l.google.com", "smtp server address")
-    var smtp_server_port = flag.Int("port", 25, "smtp server port")
-    var body = flag.String("body", "Email test", "email body")
-    var subject = flag.String("subject", "Email test", "email subject")
-    flag.Parse()
-    fmt.Printf("CONNECTING TO: %s:%d\n", *smtp_server_address, *smtp_server_port )
-    fmt.Printf("FROM: %s\n", *sender)
-    fmt.Printf("TO: %s\n", *recipient)
-    fmt.Printf("SUBJECT: %s\n", *subject)
-    fmt.Printf("BODY: %s\n", *body)
-    fmt.Printf("\n\n")
-	send(*smtp_server_address, *smtp_server_port, *sender, *recipient, *subject, *body)
+func isEmpty(str string) bool {
+	//return str == ""
+	return len(str) == 0
 }
 
-func send(smtp_server_address string, smtp_server_port int, sender string, recipient string, subject string, body string) {
+// Config : app conf
+type Config struct {
+	sender    string
+	recipient string
+	server    string
+	port      int
+	subject   string
+	body      string
+	user      string
+	password  string
+}
+
+func main() {
+
+	// default parameters
+	var conf = Config{
+		sender:    "sender@example.com",
+		recipient: "recipient@example.com",
+		server:    "smtp.example.com",
+		port:      25,
+		subject:   "Email subject",
+		body:      "Email body",
+		user:      "",
+		password:  "",
+	}
+
+	readConfFile(&conf)
+
+	parseArguments(&conf)
+
+	fmt.Printf("%+v\n", conf)
+	fmt.Printf("CONNECTING TO: %s:%d\n", conf.server, conf.port)
+	fmt.Printf("FROM: %s\n", conf.sender)
+	fmt.Printf("TO: %s\n", conf.recipient)
+	fmt.Printf("SUBJECT: %s\n", conf.subject)
+	fmt.Printf("BODY: %s\n", conf.body)
+
+	// authentication send
+	if !isEmpty(conf.user) {
+		fmt.Printf("user: %s\n", conf.user)
+		if !isEmpty(conf.password) {
+			fmt.Printf("password: ***\n")
+		}
+		fmt.Printf("\n\n")
+		sendAuth(conf.server, conf.port, conf.sender, conf.recipient, conf.subject, conf.body, conf.user, conf.password)
+		return
+	}
+
+	// without authentication
+	fmt.Printf("\n\n")
+	send(conf.server, conf.port, conf.sender, conf.recipient, conf.subject, conf.body)
+	return
+}
+
+func readConfFile(conf *Config) {
+	// read conf file
+	tomlData, err := ioutil.ReadFile("sendmail.toml")
+	if err != nil {
+		fmt.Printf("error reading configuration file:\n\t%s\n", err)
+	} else {
+		// se il file è stato letto
+		_, err := toml.Decode(string(tomlData), &conf)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func parseArguments(conf *Config) {
+	conf.sender = *flag.String("from", conf.sender, "sender email address")
+	conf.recipient = *flag.String("to", conf.recipient, "recipient email address")
+	conf.server = *flag.String("server", conf.server, "smtp server address")
+	conf.port = *flag.Int("port", conf.port, "smtp server port")
+	conf.body = *flag.String("body", conf.body, "email body")
+	conf.subject = *flag.String("subject", conf.subject, "email subject")
+	conf.user = *flag.String("user", conf.user, "authentication user")
+	conf.password = *flag.String("password", conf.password, "authentication password")
+	flag.Parse()
+}
+
+func send(smtpServerAddress string, smtpServerPort int, sender string, recipient string, subject string, body string) {
 	m := gomail.NewMessage()
-    m.SetHeader("From", sender)
-    m.SetHeader("To", recipient)
-    m.SetHeader("Subject", subject)
-    m.SetBody("text/plain", body)
+	m.SetHeader("From", sender)
+	m.SetHeader("To", recipient)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/plain", body)
 
-    d := gomail.Dialer{Host: smtp_server_address, Port: smtp_server_port}
-    if err := d.DialAndSend(m); err != nil {
-        fmt.Println(err)
-        fmt.Println("")
-        fmt.Println("")
-    }
+	d := gomail.Dialer{Host: smtpServerAddress, Port: smtpServerPort}
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		fmt.Println("")
+		fmt.Println("")
+	}
 
-    fmt.Print("Press 'Enter' to continue...")
-    bufio.NewReader(os.Stdin).ReadBytes('\n') 
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func sendAuth(smtpServerAddress string, smtpServerPort int, sender string, recipient string, subject string, body string, user string, password string) {
+	m := gomail.NewMessage()
+	m.SetHeader("From", sender)
+	m.SetHeader("To", recipient)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/plain", body)
+
+	d := gomail.NewDialer(smtpServerAddress, smtpServerPort, user, password)
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		fmt.Println("")
+		fmt.Println("")
+	}
+
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
